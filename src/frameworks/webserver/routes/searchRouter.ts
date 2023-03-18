b99785922fdcf6ffdb380c800e6b192cd13bd7b8
RokuFSD <emilianordx@gmail.com>
2023-03-18 16:40:50 -0300

    Removing mocks

--- src/frameworks/webserver/routes/searchRouter.ts
+++ src/frameworks/webserver/routes/searchRouter.ts
@@ -2,16 +2,16 @@ import { Router } from "express";
 import searchController from "../../../adapters/controllers/searchController";
 import searchRepository from "../../../application/repositories/searchRepository";
 import LocationRepository from "../../../application/repositories/locationRepository";
-import { SerpApiMock, SerpApiService } from "../../services/serpapi";
-import { IpApiMock, IpApiService } from "../../services/ipapi";
+import { SerpApiService } from "../../services/serpapi";
+import { IpApiService } from "../../services/ipapi";
 
 export default (function searchRouter() {
   const router = Router();
   const controller = searchController(
     searchRepository,
-    new SerpApiMock(),
+    new SerpApiService(),
     LocationRepository,
-    new IpApiMock()
+    new IpApiService()
   );
   router.get("/", controller.triggerSearch);
   return router;
